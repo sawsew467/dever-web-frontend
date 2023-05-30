948469fe8583441284fb5c4c1ba653a1717ee9ce
sawsew467 <bao.thang.1912@gmail.com>
2023-05-30 20:29:52 +0700

    first commit

---
+++ app/activity/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function index() {
+  console.log("!!!")
+  return (
+    <div>activity</div>
+  )
+}
+
+export default index
---
+++ components/Header/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default index
