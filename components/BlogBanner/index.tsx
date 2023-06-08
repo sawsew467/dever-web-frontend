553eab1db753159a29b195845a96480ac9a9bcf2
Hoang Huy <hoangvh230823@gmail.com>
2023-06-08 12:58:41 +0700

    create banner blog

---
+++ components/BlogBanner/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+function index() {
+  return (
+    <div className='w-[100%] h-[64px] flex justify-between items-center px-[5.5%]'>
+ 
+    </div>
+  )
+}
+
+export default index
