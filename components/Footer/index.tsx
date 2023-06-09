409c1249800f2405d79d6981c8357c51a82840b4
sawsew467 <bao.thang.1912@gmail.com>
2023-06-09 14:02:07 +0700

    fix code header, footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -8,12 +8,14 @@ import mail from "../../assets/images/footer/mail-line.svg";
 import phone from "../../assets/images/footer/phone-line.svg";
 import mapPin from "../../assets/images/footer/map-pin-line.svg";
 import fudever from "../../assets/images/footer/FU-DEVER.svg";
+import Link from "next/link";
 
-function index() {
+function Footer() {
   const data = [
     {
       logo: mail,
       tittle: "club.dever@gmail.com",
+      url: "",
     },
     {
       logo: phone,
@@ -25,48 +27,64 @@ function index() {
     },
   ];
   return (
-    <div className="w-[100%] bg-[#0098FF] h-[250px] flex px-[80px] py-[60px] justify-between text-[#fff]">
-      <div className="flex ">
-        <div className="flex flex-col justify-between pr-[200px]">
-          <div className="flex ">
-            <Image src={logodever1} alt="Picture of the author" />
-            <Image
-              src={fudever}
-              alt="Picture of the author"
-              className="pl-[8px]"
-            />
-          </div>
-          <div className="flex w-[136px] justify-between">
-            <Image src={facebookLogo} alt="Picture of the author" />
-            <Image src={githubLogo} alt="Picture of the author" />
-            <Image src={tiktokLogo} alt="Picture of the author" />
-          </div>
-        </div>
-        <div className="flex flex-col justify-between w-[310px]">
-          {data.map((item) => (
-            <div className="flex ">
+    <div className="w-screen bg-[#0098FF] h-[250px] text-[#fff]">
+      <div className="max-w-[1440px] mx-auto px-[80px] py-[60px] flex justify-between">
+        <div className="flex">
+          <div className="flex flex-col justify-between pr-[200px] gap-[38px]">
+            <a target="_blank" href="/" className="flex ">
+              <Image src={logodever1} alt="Picture of the author" />
               <Image
-                className="mr-[12px]"
-                src={item.logo}
+                src={fudever}
                 alt="Picture of the author"
+                className="pl-[8px]"
               />
-              <a className="font-normal text-[16px]">{item.tittle}</a>
+            </a>
+            <div className="flex gap-[20px]">
+              <a target="_blank" href="https://www.facebook.com/FPTUDever">
+                <Image src={facebookLogo} alt="Picture of the author" />
+              </a>
+              <a target="_blank" href="https://github.com/sawsew467">
+                <Image src={githubLogo} alt="Picture of the author" />
+              </a>
+              <a target="_blank" href="https://www.tiktok.com/@daihocfptdanang">
+                <Image src={tiktokLogo} alt="Picture of the author" />
+              </a>
             </div>
-          ))}
+          </div>
+          <div className="flex flex-col justify-between w-[310px]">
+            {data.map((item, Footer) => (
+              <div key={Footer} className="flex ">
+                <Image
+                  className="mr-[12px]"
+                  src={item.logo}
+                  alt="Picture of the author"
+                />
+                <a className="font-normal text-[16px]">{item.tittle}</a>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-      <div className="w-[240px]  flex justify-between items-start">
-        <div className="flex  justify-between flex-col">
-          <a className="mb-[18px] ">Home</a>
-          <a>Project</a>
-        </div>
-        <div className="flex  justify-between flex-col">
-          <a className="mb-[18px]">Activities</a>
-          <a>Member</a>
+        <div className="w-[240px]  flex justify-between items-start">
+          <div className="flex  justify-between flex-col">
+            <Link href="/" className="mb-[18px] hover:underline">
+              Home
+            </Link>
+            <Link href="/project" className="hover:underline">
+              Project
+            </Link>
+          </div>
+          <div className="flex  justify-between flex-col">
+            <Link href="/activity" className="mb-[18px] hover:underline">
+              Activities
+            </Link>
+            <Link href="/member" className="hover:underline">
+              Member
+            </Link>
+          </div>
         </div>
       </div>
     </div>
   );
 }
 
-export default index;
+export default Footer;
