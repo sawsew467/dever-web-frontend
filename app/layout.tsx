5f020625cdd34231ef3ba8e1387342182b9ed192
VuVuDev <vuvo070403@gmail.com>
2023-06-12 23:21:14 +0700

    vu-up-introduce-product

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Introduction from "../components/Introduction";
-import Reaching from "../components/Reaching";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
--- app/sections/home/Introduction/index.tsx
+++ app/sections/home/Introduction/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from "react";
 import Image from "next/image";
 import introduction from "@pageImage/home/introduction/introduce.png";
