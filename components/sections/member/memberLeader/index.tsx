32adc840441d680ea7c55acd369cd82d196a9327
Lê Đức Anh Phương <127871368+KICHm1@users.noreply.github.com>
2023-08-16 20:43:38 +0700

    phuong sua code responsive 768

--- components/sections/member/memberLeader/index.tsx
+++ components/sections/member/memberLeader/index.tsx
@@ -72,7 +72,7 @@ const ListLeaderMember = () => {
               <Image
                 width={290}
                 height={400}
-                className="object-cover lg:rounded-tl-[20px] lg:rounded-br-[20px] md:rounded-tl-[15px] md:rounded-br-[15px] sm:rounded-tl-[8px] sm:rounded-br-[8px]  w-[100%] h-[100%] "
+                className="object-cover lg:rounded-tl-[20px] lg:rounded-br-[20px] md:rounded-tl-[15px] md:rounded-br-[15px] sm:rounded-tl-[8px] sm:rounded-br-[8px] w-[100%] h-[100%] "
                 alt=""
                 src={avatar}
               ></Image>
@@ -82,9 +82,9 @@ const ListLeaderMember = () => {
                     K17
                   </p>
                 </div>
-                <div className=" w-[100%] xl:p-[20px] lg:p-[14px] md:pt-[5px] md:px-[10px] sm:p-[5px] xl:h-[118px] lg:h-[88px] md:h-[59px] sm:h-[37px] bg-primary lg:rounded-br-[20px]  md:rounded-br-[15px] sm:rounded-br-[4px]">
+                <div className=" w-[100%] xl:p-[20px] lg:p-[14px] md:pt-[5px] md:px-[10px] md:py-0 sm:p-[5px] xl:h-[118px] lg:h-[88px] md:h-[59px] sm:h-[37px] bg-primary lg:rounded-br-[20px]  md:rounded-br-[15px] sm:rounded-br-[4px]">
                   <div className="flex flex-col h-[100%] xl:gap-[12px] md:gap-[6px] sm:gap-[4px] ">
-                    <h4 className=" font-[700]  xl:leading-[24.2px] lg:leading-[16.94px] sm:leading-[7px] xl:text-[20px] lg:text-[14px] md:text-[10px] sm:text-[6px] text-[12px] text-[#fff] ">
+                    <h4 className=" font-[700]  xl:leading-[24.2px] lg:leading-[16.94px] md:leading-[12px] sm:leading-[7px] xl:text-[20px] lg:text-[14px] md:text-[10px] sm:text-[6px] text-[12px] text-[#fff] ">
                       Trần Văn Bảo Thắng
                     </h4>
                     <div className=" h-[auto] xl:text-[14px]  lg:text-[12px] md:text-[8px] sm:text-[5px] xl:leading-[17px] lg:leading-[14.52px] md:leading-[9.6px] sm:leading-[6px] flex justify-between items-end ">
