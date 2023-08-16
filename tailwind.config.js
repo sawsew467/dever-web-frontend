c7ef0f54d3761f40e785af861314ad33bc78a875
Lê Đức Anh Phương <127871368+KICHm1@users.noreply.github.com>
2023-08-16 11:35:53 +0700

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens : {
+      'xl' : '1440px',
+      'lg' : '1024px',
+      'md' : '768px',
+      'mobile' : '360px'
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
