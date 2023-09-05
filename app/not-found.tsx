25cf8387fc01a32cc6a4c212d0faf0290ff60a21
Lê Đức Anh Phương <127871368+KICHm1@users.noreply.github.com>
2023-09-05 16:49:31 +0700

    phuong push tat ca respon page 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -20,8 +20,8 @@ const Not_found = () => {
     onClick={(event : React.MouseEvent<HTMLElement>)=>{
       if(!input.current.contains(event.target) && input.current.innerText == "") setOpen(false)
     }}
-    className="max-w-[1440px] flex flex-col gap-[40px] mx-[auto] xl:px-[80px] md:px-[40px] sm:px-[20px] my-[120px]">
-      <div className="flex justify-between items-end md:flex-row sm:gap-[25px] md:gap-[20px] sm:flex-col ">
+    className="max-w-[1440px] flex flex-col xl:gap-[28px] lg:gap-[40px] md:gap-[35px] sm:gap-[20px] mx-[auto] xl:px-[80px] md:px-[40px] sm:px-[20px] my-[120px]">
+      <div className="flex justify-between md:items-end sm:items-start md:flex-row sm:gap-[25px] md:gap-[20px] sm:flex-col ">
         <SectionTittle
           title="Page Not Found"
           subtitle="Oops! Looks like you followed a bad link. If you think this is a problem with us, please tell us."
@@ -29,18 +29,18 @@ const Not_found = () => {
         ></SectionTittle>
         <Link
           href="/"
-          className="min-w-[137px] flex-wrap content-center h-[54px] px-[14px] flex gap-[14px] bg-primary rounded-[10px] md:items-end sm:item-start"
+          className="flex lg:gap-[16px] md:gap-[14px] sm:gap-[10px] lg:px-[14px] sm:px-[12px] rounded-[10px] lg:min-w-[137px] md:min-w-[111px] lg:h-[57px] md:h-[48px] sm:h-[48px] items-center bg-primary"
         >
-          <Image src={goHome} alt="go home" width={23} height={22} className="mt-[14px] mb-[18px]"></Image>
-          <p className=" font-[700] text-[16px] mt-[14px] mb-[16px] text-[#fff]">Go Home</p>
+          <Image src={goHome} alt="go home" width={23} height={22} className="mt-[14px] mb-[18px] lg:w-[23px] h-[auto] md:w-[20px] sm:w-[18px] aspect-[23/22]"></Image>
+          <p className="font-[500] lg:text-[16px] sm:text-[12px] whitespace-nowrap text-[#fff] mt-[14px] mb-[16px]">Go Home</p>
         </Link>
       </div>
-      <div className="flex flex-col items-center gap-[40px]">
+      <div className="flex flex-col items-center xl:gap-[28px] lg:gap-[40px] md:gap-[35px] sm:gap-[20px]">
         <div className="flex gap-[31.5px] justify-center">
           <Image src={logo404} alt="logo" width={911} height={290}></Image>
         </div>
-        <div className="flex gap-[40px] items-end">
-          <div className="relative xl:w-[760px] z-1  h-[auto] min-h-[128px] border-[#0098FF] border-[2px] rounded-[10px]">
+        <div className="flex xl:gap-[40px] w-[100%] lg:gap-[30px] md:-[20px] sm:gap-[12px] items-end">
+          <div className="relative w-[100%] z-1 h-[auto] min-h-[128px] ">
             <div ref={input}
                onKeyDown={(event : React.KeyboardEvent<HTMLDivElement>) => {
                 if (event.code == "Enter") {
@@ -51,9 +51,9 @@ const Not_found = () => {
             }
           }}  
             onClick={() =>setOpen(true)}
-            contentEditable={true}  className="cursor-pointer font-[500] text-[16px] absolute z-2 flex items-start outline-0 pl-[20px] pt-[20px] top-0 left-0 w-[100%] h-[100%] text-left ">
+            contentEditable={true}  className="cursor-pointer text-[#000] font-[500] lg:text-[16px] md:text-[14px] sm:text-[12px] border-[#0098FF] border-[2px] rounded-[10px] absolute h-[auto] min-h-[128px] max-w-[100%] outline-0 md:p-[20px] sm:p-[15px] sm:pt-[30px] top-0 left-0 w-[100%] text-left ">
             </div>
-            <p className={`absolute left-[20px] ${open ? "top-0 opacity-[0.55]" : "top-[20px] opacity-[1]"} font-[500] transition-[top,opacity] duration-[0.5s] text-[#0098FF]`}>Send me your feedback, if you think this is a our problem.</p>
+            <p className={`absolute left-[20px] ${open ? "top-0 opacity-[0.55]" : "md:top-[20px] sm:top-[30px] opacity-[1]"} font-[500] md:text-[16px] sm:text-[12px] transition-[top,opacity] duration-[0.5s] mr-[5px] text-[#0098FF]`}>Send me your feedback, if you think this is a our problem.</p>
           </div>
           <button 
             onClick={()=>{
@@ -61,9 +61,9 @@ const Not_found = () => {
               input.current.innerText="";
               setOpen(false);
             }}
-            className="flex gap-[16px] px-[14px] rounded-[10px] w-[111px] h-[57px] items-center bg-primary">
-            <Image src={send} alt="gui" width={27} height={23}></Image>
-            <p className="font-[500] text-[16px] text-[#fff]">Send</p>
+            className="flex lg:gap-[16px] md:gap-[14px] sm:gap-[10px] lg:px-[14px] sm:px-[12px] rounded-[10px] xl:w-[111px] md:w-[102px] sm:w-[104px] lg:h-[57px] md:h-[48px] sm:h-[48px] items-center bg-primary">
+            <Image src={send} alt="gui" width={27} height={23} className="lg:w-[27px] h-[auto] md:w-[25px] sm:w-[23px] aspect-[27/23]"></Image>
+            <p className="font-[500] lg:text-[16px] sm:text-[12px]  text-[#fff]">Send</p>
           </button>
         </div>
       </div>
