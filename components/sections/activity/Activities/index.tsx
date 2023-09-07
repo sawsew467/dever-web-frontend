5e952f5d432fce089c29348df45a7e16c417730a
tthienduc13 <ducnltdev@gmail.com>
2023-09-07 23:27:56 +0700

    fix slider

--- components/sections/activity/Activities/index.tsx
+++ components/sections/activity/Activities/index.tsx
@@ -40,32 +40,30 @@ function Activities() {
             ></SectionTitle>
             <div className="flex flex-col mt-[20px] md:flex-row md:gap-0 sm:gap-[30px] justify-between w-full ">
               {academicActivities.map((activity, i) => (
-                <>
-                  <div
-                    className="flex xl:w-[calc((100%-80px)/3)] md:w-[calc((100%-40px)/3)] w-full flex-col items-center gap-4"
-                    key={i}
-                  >
-                    <Image
-                      src={activity.img}
-                      alt={activity.alt}
-                      className=" object-cover xl:w-[400px] xl:h-[320px] lg:w-[283px] lg:h-[252px] md:w-[200px]  md:h-[178px] sm:w-full sm:h-[270px]"
-                    ></Image>
-                    <div className="flex flex-col items-center max-w-[277px] w-full">
-                      <p className="lg:text-[40px] text-[#0065A9] font-bold lg:leading-[48px] md:text-[24px] md:leading-[29px] sm:leading-[48px] sm:text-[40px] mb-2">
-                        {activity.title}
+                <div
+                  key={i}
+                  className="flex xl:w-[calc((100%-80px)/3)] md:w-[calc((100%-40px)/3)] w-full flex-col items-center gap-4"
+                >
+                  <Image
+                    src={activity.img}
+                    alt={activity.alt}
+                    className=" object-cover xl:w-[400px] xl:h-[320px] lg:w-[283px] lg:h-[252px] md:w-[200px]  md:h-[178px] sm:w-full sm:h-[270px]"
+                  ></Image>
+                  <div className="flex flex-col items-center max-w-[277px] w-full">
+                    <p className="lg:text-[40px] text-[#0065A9] font-bold lg:leading-[48px] md:text-[24px] md:leading-[29px] sm:leading-[48px] sm:text-[40px] mb-2">
+                      {activity.title}
+                    </p>
+                    <p className="leading-[19px] lg:max-w-[277px] md:max-w-[188px] sm:w-full text-[16px] font-light text-center mb-[20px]">
+                      {activity.desc}
+                    </p>
+                    <button className=" flex gap-1 flex-row items-center lg:px-[32px] lg:py-2 md:px-[10px] md:py-1 px-[32px] py-2 bg-[#0098FF] rounded-[2px]">
+                      <p className="font-normal text-[16px] leading-[24px] text-white">
+                        View more
                       </p>
-                      <p className="leading-[19px] lg:max-w-[277px] md:max-w-[188px] sm:w-full text-[16px] font-light text-center mb-[20px]">
-                        {activity.desc}
-                      </p>
-                      <button className=" flex gap-1 flex-row items-center lg:px-[32px] lg:py-2 md:px-[10px] md:py-1 px-[32px] py-2 bg-[#0098FF] rounded-[2px]">
-                        <p className="font-normal text-[16px] leading-[24px] text-white">
-                          View more
-                        </p>
-                        <Image src={ArrowIcon} alt="arrowIcon"></Image>
-                      </button>
-                    </div>
+                      <Image src={ArrowIcon} alt="arrowIcon"></Image>
+                    </button>
                   </div>
-                </>
+                </div>
               ))}
             </div>
           </div>
--- components/sections/activity/Slider/index.tsx
+++ components/sections/activity/Slider/index.tsx
@@ -8,76 +8,73 @@ interface ExtraActivity {
 function Slider() {
   const extraActivities: ExtraActivity[] = [
     {
-      img: "https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?cs=srgb&dl=pexels-pixabay-326055.jpg&fm=jpg",
+      img: "https://scontent.fsgn2-8.fna.fbcdn.net/v/t39.30808-6/349050689_953975449058653_489753596979940785_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=49d041&_nc_ohc=xTrZpaL-1G0AX-vDIsA&_nc_ht=scontent.fsgn2-8.fna&oh=00_AfBMOAFQduOwAFPuF1VTE2jKuDrKzXzDx0zLycOqRi-Rzw&oe=64FE127A",
       alt: "img",
     },
     {
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT70WgKN0lItPR2lfxTLxbIEQv9X2QuaFpFSG9Tqb_jww&s",
+      img: "https://scontent.fsgn2-9.fna.fbcdn.net/v/t39.30808-6/349054842_214719474684619_8552380635753328268_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=49d041&_nc_ohc=Gir-YbjKfrYAX_-8RJw&_nc_ht=scontent.fsgn2-9.fna&oh=00_AfCYsQXgELTGYCNIOqjJl_N9yC0oGRPohYmhpQbLbXpmnQ&oe=64FE4DDC",
       alt: "img",
     },
     {
-      img: "https://images.unsplash.com/photo-1509043759401-136742328bb3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80",
+      img: "https://scontent.fsgn2-6.fna.fbcdn.net/v/t39.30808-6/338861507_243695788017140_906781670802621311_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=49d041&_nc_ohc=9n7C2Ya_TUsAX-mcRgY&_nc_oc=AQnlSDLroX51VhK7DlFG-u_lVOH2IkFBirrqZCPvaFgER5J0U4Im5TjGxECxSDhPtoiTVX4GIykv_jB4AMHa89zi&_nc_ht=scontent.fsgn2-6.fna&oh=00_AfD69Ghl01UEgvmGwKGObQhTOWqebCPgMaaGWAnj3F3raw&oe=64FE3824",
       alt: "img",
     },
     {
-      img: "https://thumbs.dreamstime.com/b/two-hummingbird-bird-pink-flower-hummingbirds-fiery-throated-hummingbird-flying-next-to-beautiful-bloom-flower-savegre-co-95610310.jpg",
+      img: "https://scontent.fsgn2-9.fna.fbcdn.net/v/t39.30808-6/338865895_3532348523756010_9051564565765525231_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=49d041&_nc_ohc=sYw3WZDSGXwAX-lfUi8&_nc_oc=AQmlNgxnJZuzyetDeL_P7DcjpDsGPr6RuGlTEL4O2uSqJ5aSyAecWXc-b8wvM-EPR0EHZcBHDdS6uJABc1b97fxM&_nc_ht=scontent.fsgn2-9.fna&oh=00_AfA8KvGUw9Deklvzu8RzHLps0vvw9TyPGc6ky-6fTseFVg&oe=64FE3E9B",
       alt: "img",
     },
     {
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgVH4td5yK_J3I8HD986yGhhDsk2rrnwUcPxye4QI-9z4fmBmDXVjRbTVlHlgQNH_YFDc&usqp=CAU",
+      img: "https://scontent.fsgn2-8.fna.fbcdn.net/v/t39.30808-6/338827797_922595032123733_5176784466139281130_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=49d041&_nc_ohc=3SzKSIhEsoIAX-29BIQ&_nc_ht=scontent.fsgn2-8.fna&oh=00_AfB_9BKqfIQf1WJVqCI3clftuB6uMFlFNsqdVmq-clXujQ&oe=64FF6559",
       alt: "img",
     },
     {
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYDQ6bZVzCwxR4vlDZjm-P7NNszby-vIn3japtt81kPKGDZ96yiBl-FdfHOLwPXaQeTJg&usqp=CAU",
+      img: "https://scontent.fsgn2-7.fna.fbcdn.net/v/t39.30808-6/338937141_247877991011574_5482980115593587490_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=49d041&_nc_ohc=MyCS4okn2dEAX9pM0ge&_nc_ht=scontent.fsgn2-7.fna&oh=00_AfAJOR4cGc_pHds4N89z-XLg64FG0w8FLoVFlKX1iHQuhQ&oe=64FFA029",
       alt: "img",
     },
     {
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoy_PaAPmEKRK_V6AIa3ucopcTzWY_bAa88A&usqp=CAU",
+      img: "https://scontent.fsgn2-9.fna.fbcdn.net/v/t39.30808-6/339031288_744871543974045_97018292859331667_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=49d041&_nc_ohc=hFAo2ngZF2AAX9VfC0j&_nc_ht=scontent.fsgn2-9.fna&oh=00_AfDy3rNX8waQs-Er9Z6PrF-FapjWtqPTogUceBW8rw6Syg&oe=64FE3FAE",
       alt: "img",
     },
     {
-      img: "https://wallpapers.com/images/featured/beautiful-painting-31jw65zmfdepcygw.jpg",
+      img: "https://scontent.fsgn2-3.fna.fbcdn.net/v/t39.30808-6/338587375_179049917855651_3139915445004340605_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=49d041&_nc_ohc=R14-tgtsyRcAX_O-14u&_nc_ht=scontent.fsgn2-3.fna&oh=00_AfAh-_mKG9AtKJz0NOz_opQMntKrcTa0obMgtO3g1saLsw&oe=64FF39C3",
       alt: "img",
     },
     {
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMASWkcGJ0mrs_mPsde0thFAtmvfMyH0Tidg&usqp=CAU",
+      img: "https://scontent.fsgn2-7.fna.fbcdn.net/v/t39.30808-6/338707645_1235965160353007_8437435302741970211_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=49d041&_nc_ohc=TYOh4EH9tV0AX9EcAQh&_nc_ht=scontent.fsgn2-7.fna&oh=00_AfDDzwJwnPvzE71YRFJmEOR3gaDegDyDXEW3c4UOn98_kA&oe=64FFACCF",
       alt: "img",
     },
     {
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1pkcNsYi13Myhj9l5SgjhLlYh9OIm_L9_1w&usqp=CAU",
+      img: "https://scontent.fsgn2-6.fna.fbcdn.net/v/t39.30808-6/338928837_907931357095705_5497796432846645343_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=49d041&_nc_ohc=LuT6T4inWXUAX-PiI1F&_nc_ht=scontent.fsgn2-6.fna&oh=00_AfBY6RQXo3Hy-mp_gVTbAPi11vbxUCUvAGscJjZaAMIhEg&oe=64FF45EC",
       alt: "img",
     },
     {
-      img: "https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014_1280.jpg",
+      img: "https://scontent.fsgn2-9.fna.fbcdn.net/v/t39.30808-6/339130867_3579406705612877_7161432628448574231_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=49d041&_nc_ohc=zZ9faTGFSscAX9-5Hi5&_nc_ht=scontent.fsgn2-9.fna&oh=00_AfARrruLu7GoFDwUoLDMWFrSYpG_oO0aBmHwGtcbQ3SXLg&oe=64FEA1DA",
       alt: "img",
     },
     {
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV59vWRafpqhV8ScX0XuiDkHIU_AIJlTiVFg&usqp=CAU",
+      img: "https://scontent.fsgn2-3.fna.fbcdn.net/v/t39.30808-6/339277047_3147849108840632_2716527022012185335_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=49d041&_nc_ohc=OVmkpaBbnYUAX8SmKkG&_nc_ht=scontent.fsgn2-3.fna&oh=00_AfBW1qBop-Y_x2hhgIXpIjIBuTMv6UsjKyWMunz9TJHgbg&oe=64FE263D",
+      alt: "img",
+    },
+    {
+      img: "https://scontent.fsgn2-5.fna.fbcdn.net/v/t39.30808-6/338927024_3499335590286414_3718941471594868781_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=49d041&_nc_ohc=-EcD6KKv7JwAX_Jzaos&_nc_ht=scontent.fsgn2-5.fna&oh=00_AfAByyvXpj7kHak0_rh7lasmSWEL6E7avZroQI6Ujv-n_A&oe=64FDFF2C",
       alt: "img",
     },
   ];
   const maxVisibleImages = 4;
-  const [indexStart, setIndexStart] = useState<number>(0);
-  const [indexEnd, setIndexEnd] = useState<number>(maxVisibleImages - 1);
+
   const [selectedIndex, setSelectedIndex] = useState<number>(0);
 
   const handleImage = (index: number) => {
     setSelectedIndex(index);
-    setIndexStart(index);
-    setIndexEnd((index + maxVisibleImages - 1) % extraActivities.length);
   };
 
   const handleNextImg = () => {
-    setIndexStart((indexStart + 1) % extraActivities.length);
-    setIndexEnd((indexEnd + 1) % extraActivities.length);
+    setSelectedIndex((selectedIndex + 1) % extraActivities.length);
   };
 
   const handlePreviousImg = () => {
-    setIndexStart(
-      (indexStart - 1 + extraActivities.length) % extraActivities.length
-    );
-    setIndexEnd(
-      (indexEnd - 1 + extraActivities.length) % extraActivities.length
+    setSelectedIndex(
+      (selectedIndex - 1 + extraActivities.length) % extraActivities.length
     );
   };
   return (
@@ -101,8 +98,8 @@ function Slider() {
               </svg>
               <img
                 className="w-4/5 h-full object-cover"
-                src={extraActivities[indexStart]?.img}
-                alt={extraActivities[indexStart]?.alt}
+                src={extraActivities[selectedIndex]?.img}
+                alt={extraActivities[selectedIndex]?.alt}
               />
               <svg
                 onClick={handleNextImg}
@@ -121,25 +118,37 @@ function Slider() {
           </div>
           <div className="w-full mt-[20px] flex gap-[20px] items-center">
             <div className="w-full justify-between flex items-center">
-              {extraActivities.map((img, index) => {
-                if (
-                  indexStart <= indexEnd
-                    ? index >= indexStart && index <= indexEnd
-                    : index >= indexStart || index <= indexEnd
-                ) {
+              {extraActivities.map((image, index) => {
+                const isSelected = index === selectedIndex;
+                const isInRange =
+                  index >= selectedIndex - 1 && index <= selectedIndex + 2;
+                const shouldBeDisplay =
+                  isSelected ||
+                  isInRange ||
+                  (selectedIndex === 0 &&
+                    index >= selectedIndex &&
+                    index <= selectedIndex + 3) ||
+                  (selectedIndex === extraActivities.length - 2 &&
+                    index >= selectedIndex - 2 &&
+                    index <= selectedIndex) ||
+                  (selectedIndex === extraActivities.length - 1 &&
+                    index >= selectedIndex - 3 &&
+                    index <= selectedIndex);
+                if (shouldBeDisplay) {
                   return (
                     <img
                       onClick={() => handleImage(index)}
                       className={`cursor-pointer ${
-                        indexStart === index ? "blur-0" : "blur-[2px]"
+                        isSelected ? "blur-0" : "blur-[2px]"
                       } md:w-[calc((100%-60px)/4)] sm:w-[calc((100%-30px)/4)] max-h-[200px] h-full object-cover`}
                       key={index}
-                      src={img.img}
-                      alt={img.alt}
+                      src={image.img}
+                      alt={image.alt}
                     />
                   );
+                } else {
+                  return null;
                 }
-                return null;
               })}
             </div>
           </div>
