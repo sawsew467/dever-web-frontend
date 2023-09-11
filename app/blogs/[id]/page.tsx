4b582e4173b7e71acc27edb6157fa1228004ebf8
sawsew467 <bao.thang.1912@gmail.com>
2023-09-11 08:10:47 +0700

    comment fs, gray-matter

--- app/blogs/[id]/page.tsx
+++ app/blogs/[id]/page.tsx
@@ -4,36 +4,36 @@ import Banner from "@/components/sections/post/Banner";
 import Body from "@/components/sections/post/Body";
 import Author from "@/components/sections/post/Author";
 import RelatedPost from "@/components/sections/post/RelatedPost";
-import fs from "fs";
-import matter from "gray-matter";
-import getPostMetadata from "@/utils/getPostMetadata";
+// import fs from "fs";
+// import matter from "gray-matter";
+// import getPostMetadata from "@/utils/getPostMetadata";
 
 
-export async function generateStaticParams() {
-  const posts = getPostMetadata();
-  return posts.map((post) => ({
-    id: post.id,
-  }));
-}
+// export async function generateStaticParams() {
+//   const posts = getPostMetadata();
+//   return posts.map((post) => ({
+//     id: post.id,
+//   }));
+// }
 
-const getPostContent = (id: string) => {
-  try {
-  const folder = "posts/";
-  const file = `${folder}${id}.md`;
+// const getPostContent = (id: string) => {
+//   try {
+//   const folder = "posts/";
+//   const file = `${folder}${id}.md`;
   
-  const content = fs.readFileSync(file, "utf8");
-  const matterResult = matter(content);
+//   const content = fs.readFileSync(file, "utf8");
+//   const matterResult = matter(content);
 
-  return matterResult;
-  } catch (error) {
-    return null 
-  }
-};
+//   return matterResult;
+//   } catch (error) {
+//     return null 
+//   }
+// };
 
 
 export default function Page({ params }: { params: { id: string } }) {
   const id = params.id;
-  const post = getPostContent(id);
+  // const post = getPostContent(id);
   
   return (
     <div>
--- utils/getPostMetadata.tsx
+++ utils/getPostMetadata.tsx
@@ -1,5 +1,5 @@
-import fs from "fs";
-import matter from "gray-matter";
+// import fs from "fs";
+// import matter from "gray-matter";
 
 export interface PostMetadata {
     title: string;
@@ -8,23 +8,23 @@ export interface PostMetadata {
     id: string;
   }
 
-const getPostMetadata = (): PostMetadata[] => {
-  const folder = "posts/";
-  const files = fs.readdirSync(folder);
-  const markdownPosts = files.filter((file) => file.endsWith(".md"));
+// const getPostMetadata = (): PostMetadata[] => {
+//   const folder = "posts/";
+//   const files = fs.readdirSync(folder);
+//   const markdownPosts = files.filter((file) => file.endsWith(".md"));
 
-  const posts = markdownPosts.map((fileName) => {
-    const fileContents = fs.readFileSync(`posts/${fileName}`, "utf8");
-    const matterResult = matter(fileContents);
-    return {
-      title: matterResult.data.title,
-      date: matterResult.data.date,
-      subtitle: matterResult.data.subtitle,
-      id: fileName.replace(".md", ""),
-    };
-  });
+//   const posts = markdownPosts.map((fileName) => {
+//     const fileContents = fs.readFileSync(`posts/${fileName}`, "utf8");
+//     const matterResult = matter(fileContents);
+//     return {
+//       title: matterResult.data.title,
+//       date: matterResult.data.date,
+//       subtitle: matterResult.data.subtitle,
+//       id: fileName.replace(".md", ""),
+//     };
+//   });
 
-  return posts;
-};
+//   return posts;
+// };
 
-export default getPostMetadata;
+// export default getPostMetadata;
