075611987ad309b7f0579ec167e07bcd8c5304fc
VuVuDev <vuvo070403@gmail.com>
2023-09-11 23:21:18 +0700

    vu-reloader-animate

--- app/activity/page.tsx
+++ app/activity/page.tsx
@@ -1,13 +1,24 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Banner from "@/components/sections/activity/Banner";
 import Activities from "@/components/sections/activity/Activities";
-function activity() {
+import Reloading from "@/components/Reloading";
+function Activity() {
+  const [isReloading, setIsReloading] = useState<boolean>(true);
+  console.log(isReloading);
+  
+  useEffect(() => {
+    setTimeout(() => {
+      setIsReloading(false);
+    }, 2400);
+  });
   return (
     <>
+      {isReloading ? <Reloading></Reloading> : null}
       <Banner></Banner>
       <Activities></Activities>
     </>
   );
 }
 
-export default activity;
+export default Activity;
---
+++ components/Reloading/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style.css'
+
+function Reloading() {
+    return (
+        <div className='fixed top-0 w-full h-full z-[1000] animate-fade-down-large'>
+            <div className='flex h-full justify-center items-center bg-primary'>
+               <div className='content flex justify-center items-center w-full animate-scale'>
+                <h2 className='text-[32px] md:text-[64px] lg:text-[64px] xl:text-[64px] text-white font-extrabold relative text-transparent'>FU-DEVER</h2>
+                    <h2 className='text-[32px] md:text-[64px] lg:text-[64px] xl:text-[64px] text-white font-extrabold absolute'>FU-DEVER</h2>
+               </div>
+            </div>
+        </div>
+    )
+}
+
+export default Reloading
