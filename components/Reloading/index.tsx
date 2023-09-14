c39f133f9ab4ec353e423d0f81dc86409b03ace5
VuVuDev <vuvo070403@gmail.com>
2023-09-14 12:49:25 +0700

    vu

--- components/Reloading/index.tsx
+++ components/Reloading/index.tsx
@@ -3,8 +3,9 @@ import React, { useEffect, useState } from "react";
 import "./style.css";
 
 function Reloading() {
+
   return (
-    <div className="fixed top-0 w-full h-full z-[1000] "
+    <div className={`fixed top-0 w-full h-full z-[1000] animation-turn-off`}
     >
       <div className="flex h-full justify-center items-center bg-primary animate-fade-down-large">
         <div className="content flex justify-center items-center w-full animate-scale">
