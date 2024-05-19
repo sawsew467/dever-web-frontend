d499c1e4f61b74e55af27b6b15fddb16685148d9
Lê Đức Anh Phương <127871368+KICHm1@users.noreply.github.com>
2024-05-19 20:14:28 +0700

    sua loi

--- app/member/[id]/page.tsx
+++ app/member/[id]/page.tsx
@@ -4,13 +4,13 @@ import { notFound } from "next/navigation";
 import axios from "axios";
 import { userEndpoint } from "@/helpers/endpoint";
 
-async function fetchUser(id: string) {
+async function fetchUser(id?: string) {
   const res = await axios.get(`${userEndpoint.GET_ALL_USERS}/${id}`);
   if (!res?.data?.data) return undefined;
   return res?.data?.data;
 }
 
-const Member = async ({ params: { id } }: { params: { id: string } }) => {
+const Member = async ({ params: { id } }: { params: { id?: string } }) => {
   const user = await fetchUser(id);
 
   if (!user) {
