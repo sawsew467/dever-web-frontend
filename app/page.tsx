9fe94a3b8976f36ad11e77cec97cbebf159c44cc
Tran Van Bao Thang <58736315+sawsew467@users.noreply.github.com>
2024-05-21 10:21:16 +0700

    feat: meta (#69)
    Co-authored-by: sawsew467 <mac@MacBook-Pro.local>

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
+import { Metadata } from "next";
+
 import Banner from "@/components/sections/home/Banner";
 import Product from "@/components/sections/home/Product";
 import Introduction from "@/components/sections/home/Introduction";
 import Join from "@/components/sections/home/Join";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "FU-DEVER | Home page",
   description:
     "Welcome to FU-DEVER, the programming club of FPT University! . At FU-DEVER, we strive to foster a vibrant community of aspiring programmers and provide a platform for skill development and collaboration.",
   icons: {
     icon: "/icons/layout/logo.png",
+  },
+  openGraph: {
+    images: ["/images/meta.jpg"],
+    title: "FU-DEVER | Câu lạc bộ lập trình FU-DEVER",
+    description:
+      "Welcome to FU-DEVER, the programming club of FPT University! . At FU-DEVER, we strive to foster a vibrant community of aspiring programmers and provide a platform for skill development and collaboration.",
   },
 };
 
